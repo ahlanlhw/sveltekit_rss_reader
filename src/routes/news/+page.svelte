4f498e2721f7b96f36fc4cc5f63d6e02937fe453
Alan Lee <44397602+ahlanlhw@users.noreply.github.com>
2023-08-22 17:30:48 +0800

    Update +page.svelte

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -1,14 +1,28 @@
 <script>
     /** @type {import('./$types').PageData} */
     export let data;
-    console.log(data.response)
+    // console.log(data.response)
 </script>
-<div class='grid v-screen place-items-center p-3'>
-    {#each data.response as k}
-    <ol type ='1' class ='p-1'>
-        <li>
-            <a href={k.link}>{k.title}</a>
-        </li>
-    </ol>    
-    {/each}
+<div class='flex flex-wrap m-3 p-3 h-screen w-screen overscroll-y-none'>
+    <div class='h-3/4 p-3 overflow-y-scroll'>
+        {#each data.response as k}
+        <div class = "border-t border-r border-b border-l border-gray-400 lg:border-l lg:border-t lg:border-gray-400 rounded-t lg:rounded-t rounded-b lg:rounded-b lg:rounded-r m-3 p-3 flex flex-col justify-between leading-normal">
+            <ol type ='1' class ='p-3'>
+                <li>
+                    <div>
+                        <a class = 'text-lg' href={k.link}>{k.title}</a>
+                        <p class = 'text-xs'>{k.pubDate}</p>
+                        <span class='text-xs break-words'>
+                            {k.description.replace( /(<([^>]+)>)/ig, '')}
+                        </span> 
+                    </div>
+                </li>
+            </ol>    
+        </div>
+        {/each}
+    </div>
+    <div>
+        <iframe title="Libsyn Player" style="border: none" src="//html5-player.libsyn.com/embed/episode/id/27797622/height/90/theme/custom/thumbnail/yes/direction/forward/render-playlist/no/custom-color/000000/" height="90" width="100%" scrolling="no"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+    </div>
+
 </div>
